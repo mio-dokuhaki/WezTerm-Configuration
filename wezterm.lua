d1107dbf9555f83af2f2a4908e9ed3c79ae05f8d
mio-dokuhaki <a214mm040s@std.kurume-u.ac.jp>
2025-01-11 19:08:42 +0900

    Update configuration

--- wezterm.lua
+++ wezterm.lua
@@ -11,13 +11,13 @@ return {
 	enable_scroll_bar = true,
 	min_scroll_bar_height = "2cell",
 	window_close_confirmation = "NeverPrompt",
-	window_background_image = wezterm.home_dir .. "Nn38tvd5-wallha.com.jpg",
+	window_background_image = wezterm.home_dir .. "kali-linux.jpg",
 	default_cursor_style = "BlinkingBar",
 	hide_mouse_cursor_when_typing = true,
 	background = {
 		{
 			source = {
-				File = "/Users/mio-dokuhaki/Nn38tvd5-wallha.com.jpg",
+				File = "/Users/mio-dokuhaki/kali-linux.jpg",
 			},
 			opacity = 0.40,
 		},
